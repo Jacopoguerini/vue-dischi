a7b53855e173ae9f15305d24453224df454e6d42
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-06-09 16:22:59 +0200

    Tentativo fallito di caricamento delle immagini

---
+++ src/components/Album.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="album">
+        <img class="img-fluid" :src="item.poster" :alt="item.title">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Album",
+    props: [ "item" ]
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../style/variables.scss';
+
+    .album {
+        height: 100px;
+        background-color: chartreuse;
+    }
+
+</style>
--- src/components/AlbumList.vue
+++ src/components/AlbumList.vue
@@ -2,7 +2,9 @@
   <section class="albums container">
 
     <div class="row">
-        
+        <div class="column" v-for="(album, index) in albums" :key="index">
+            <Album :item="album"/>
+        </div>
 
     </div>
 
@@ -10,21 +12,43 @@
 </template>
 
 <script> 
+import Album from './Album';
+import axios from 'axios';
 
 export default {
     name: "AlbumList",
     components: {
+        Album
+    },
+    data: function() {
+        return {
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+            albums: []
+        }
+    },
+    created: function() {
+        axios
+            .get(this.apiUrl)
+            .then(
+               (response) => {
+                this.albums = response.data;
+               }
+            )
+            .catch();
     }
-
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 @import '../style/variables.scss';
 
 section {
     background-color: red;
     height: 500px;
+
+    .column {
+        width: calc((100% / 5) - 25px);
+    }
 }
 
 
